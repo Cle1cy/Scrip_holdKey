2d54b6e40399563f0f4143099b112720ee2689fc
Cle1cy <psydiego1@gmail.com>
2023-10-09 23:17:46 -0500

    no se porque funciona solo el release pero si

--- main.py
+++ main.py
@@ -3,27 +3,19 @@ from pynput.keyboard import Controller
 
 keyboard = Controller()
 
-q_pressed = False  # Variable para rastrear el estado de la tecla 'q'
-
 def parse_key(key):
     key = str(key)
     key = key[1]
     return key
 
 def on_press(key):
-    global q_pressed
-    if parse_key(key) == 'q' and not q_pressed:
-        keyboard.press('q')
-        q_pressed = True
-
+    if parse_key(key) == 'q':
+        keyboard.release('q')
+        
 def on_release(key):
-    global q_pressed
     print("se paró de presionar " + parse_key(key))
     if key == kb.Key.esc:
         return False
-    if parse_key(key) == 'q' and q_pressed:
-        keyboard.release('q')
-        q_pressed = False
     
 with kb.Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
