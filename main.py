80c57aad5e5054a5a72db28a927f1e7bdbe7cef4
Cle1cy <psydiego1@gmail.com>
2023-10-09 22:58:31 -0500

    Funcionamiento de las q con alluda de caht gpt

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 from pynput import keyboard as kb
-from pynput.keyboard import Key, Controller
+from pynput.keyboard import Controller
 
 keyboard = Controller()
+
+q_pressed = False  # Variable para rastrear el estado de la tecla 'q'
 
 def parse_key(key):
     key = str(key)
     key = key[1]
     return key
 
-def press(key):
-    if parse_key(key) == 'q':
+def on_press(key):
+    global q_pressed
+    if parse_key(key) == 'q' and not q_pressed:
         keyboard.press('q')
-    else:
-        exit()
-    
-        
-def stop(key):
+        q_pressed = True
+
+def on_release(key):
+    global q_pressed
     print("se paró de presionar " + parse_key(key))
+    if key == kb.Key.esc:
+        return False
+    if parse_key(key) == 'q' and q_pressed:
+        keyboard.release('q')
+        q_pressed = False
     
-with kb.Listener(press, stop) as listener:
+with kb.Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
